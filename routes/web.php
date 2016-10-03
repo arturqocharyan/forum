3454672bdd467b3ead589b7908a55d1e7d65f5f4
arturqocharyan <artur.qocharyan@coderiders.am>
2016-10-03 13:44:25 +0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+
+    return view('welcome');
+
+});
+
+Route::get('/_debugbar/assets/stylesheets', [
+    'as' => 'debugbar-css',
+    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@css'
+]);
+
+Route::get('/_debugbar/assets/javascript', [
+    'as' => 'debugbar-js',
+    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@js'
+]);
+
+Route::get('/_debugbar/open', [
+    'as' => 'debugbar-open',
+    'uses' => '\Barryvdh\Debugbar\Controllers\OpenController@handler'
+]);
+
+
+
+
