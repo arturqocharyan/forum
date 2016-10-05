9f21b68a5e87a4ded5c6e0ab29534cc1e7ca3ff1
arturqocharyan <artur.qocharyan@coderiders.am>
2016-10-05 12:02:36 +0400

    test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,27 +9,22 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
 Route::get('/', function () {
-
     return view('welcome');
-
 });
-
 Route::get('/_debugbar/assets/stylesheets', [
     'as' => 'debugbar-css',
     'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@css'
 ]);
-
 Route::get('/_debugbar/assets/javascript', [
     'as' => 'debugbar-js',
     'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@js'
 ]);
-
 Route::get('/_debugbar/open', [
     'as' => 'debugbar-open',
     'uses' => '\Barryvdh\Debugbar\Controllers\OpenController@handler'
 ]);
+
 
 
 
